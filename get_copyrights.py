12d9edd1b2ca7a0022a158c77b5089097d4e5456
Praisy-Mathew <praissymathew987@gmail.com>
2026-06-30 00:23:40 +0530

    eighteenth commit

--- get_copyrights.py
+++
@@ -1,5 +0,0 @@
-import re
-html = open('index.html', 'r', encoding='utf-8').read()
-match = re.search(r'<div class="copyrights"[^>]*>.*?</div>', html, re.DOTALL | re.IGNORECASE)
-if match:
-    print(match.group(0))
